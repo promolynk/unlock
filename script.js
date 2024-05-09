0d9c6ee514ee1bd537d0b3fee86d3029f72283af
Umar <umr.farq@gmail.com>
2024-05-09 21:28:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('enterCodeForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    let secretCode = document.getElementById('secretCode').value;
+
+    // Fetch the list of valid codes from the Gist
+    fetch('https://gist.githubusercontent.com/im-umar/ba96e47bfa2f3dd0bdc22969f72bea87/raw/')
+        .then(response => response.text())
+        .then(data => {
+            let validCodes = data.split('\n');
+
+            // Check if the entered code is in the list of valid codes
+            if (validCodes.includes(secretCode)) {
+                // If the code is valid, redirect to the URL
+                window.location.href = 'https://taut.biz.id/' + secretCode;
+            } else {
+                // If the code is not valid, display an error message and clear the input field
+                alert('Invalid code. Please try again.');
+                document.getElementById('secretCode').value = '';
+            }
+        });
+});
+
+

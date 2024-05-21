883b5ab04c911118517c0d70c2c32a0a0c676f32
Umar <umr.farq@gmail.com>
2024-05-21 01:25:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     nextCodeButton.addEventListener('click', function() {
         const currentInput = formSteps[currentStep].querySelector('input');
-        const fieldName = currentInput.id === 'CODE' ? 'code' : '';
+        const fieldName = currentInput.id === 'CODE' ? 'discount code' : '';
         if (!validateInput(currentInput, fieldName)) {
             return;
         }

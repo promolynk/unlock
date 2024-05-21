0497dd62fc72cfb80a177fec4f103661ff855ae7
Umar <umr.farq@gmail.com>
2024-05-21 19:57:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -92,13 +92,21 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // Add an event listener for the 'pageshow' event
-    window.addEventListener('pageshow', function(event) {
+        window.addEventListener('pageshow', function(event) {
         if (event.persisted) {
-            window.location.reload();
-        } else {
-            checkLocalStorage();
+            // Check if the user is on a mobile device
+            const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    
+            // Reload the page if it's a mobile device
+            if (isMobile) {
+                window.location.reload();
+            } else {
+                // Call the function to set focus on the input field
+                checkLocalStorage();
+            }
         }
     });
+
 
     function validateInput(input, fieldName) {
         const trimmedValue = input.value.trim();
@@ -141,70 +149,64 @@ document.addEventListener("DOMContentLoaded", function() {
         codeInput.value = '';
     }
 
-async function validateForm() {
-    const name = document.getElementById('NAME').value.trim();
-    const email = document.getElementById('EMAIL').value.trim();
-    const codeInput = document.getElementById('CODE');
-    const code = codeInput.value.trim();
+    async function validateForm() {
+        const name = document.getElementById('NAME').value.trim();
+        const email = document.getElementById('EMAIL').value.trim();
+        const codeInput = document.getElementById('CODE');
+        const code = codeInput.value.trim();
 
-    if (!validateEmail(email)) {
-        return;
-    }
+        if (!validateEmail(email)) {
+            return;
+        }
 
-    try {
-        const response = await fetch('https://gist.githubusercontent.com/pixelpyro/ba96e47bfa2f3dd0bdc22969f72bea87/raw/');
-        const data = await response.text();
-        const validCodes = data.split('\n');
+        try {
+            const response = await fetch('https://gist.githubusercontent.com/pixelpyro/ba96e47bfa2f3dd0bdc22969f72bea87/raw/');
+            const data = await response.text();
+            const validCodes = data.split('\n');
 
-        if (validCodes.includes(code)) {
-            const formData = new FormData();
-            formData.append('NAME', name);
-            formData.append('EMAIL', email);
-            formData.append('CODE', code);
+            if (validCodes.includes(code)) {
+                const formData = new FormData();
+                formData.append('NAME', name);
+                formData.append('EMAIL', email);
+                formData.append('CODE', code);
 
-            codeInput.classList.add('success');
-            // Add a transition class to enable CSS transitions
-            codeInput.classList.add('transition');
+                codeInput.classList.add('success');
+                // Add a transition class to enable CSS transitions
+                codeInput.classList.add('transition');
 
-            // Set placeholder and value after a short delay to allow transition effect
-            setTimeout(() => {
-                codeInput.placeholder = 'Success, Redirecting you...';
-                codeInput.value = '';
+                // Set placeholder and value after a short delay to allow transition effect
+                    codeInput.placeholder = 'Success, Redirecting you...';
+                    codeInput.value = '';
 
-                // Remove focus from the input field
-                codeInput.blur();
-
-                // Reset fields just before redirecting
-                setTimeout(() => {
+                //resetInputFields(); // Resetting all input fields to default placeholders
                     document.activeElement.blur();
                     window.location.href = 'https://www.ishortn.ink/' + code;
-                }, 2000); // Adjust the delay as needed
-            }, 0); // Adjust the delay as needed
+      
 
-            // Submit the form data to Brevo
-            fetch('https://fc17af9f.sibforms.com/serve/MUIFAJrCl1rqwbvqTuDl1_SHLR6vl0oCI77i0ACJidsDAtxiA7LX6zTxucsOjHtc0RbeeeQilSqKzgPCMkJrcrPuuQTG_CsTUQsqZfH1t4n37YXEfTkO4Qin2o-Yb5RkDMJ0ZchoztnZqajCFloSyfDZ-E0TnNnznjp1aHd0V8bwEANogygfddtJFECq_NmxwSl9uMCLdAE4iJ7U', {
-                method: 'POST',
-                body: formData,
-                mode: 'no-cors' // Set mode to 'no-cors' to disable CORS
-            })
-            .then(response => {
-                if (!response.ok) {
-                    console.error('Error:', response.statusText);
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+                // Submit the form data to Brevo
+                fetch('https://fc17af9f.sibforms.com/serve/MUIFAJrCl1rqwbvqTuDl1_SHLR6vl0oCI77i0ACJidsDAtxiA7LX6zTxucsOjHtc0RbeeeQilSqKzgPCMkJrcrPuuQTG_CsTUQsqZfH1t4n37YXEfTkO4Qin2o-Yb5RkDMJ0ZchoztnZqajCFloSyfDZ-E0TnNnznjp1aHd0V8bwEANogygfddtJFECq_NmxwSl9uMCLdAE4iJ7U', {
+                    method: 'POST',
+                    body: formData,
+                    mode: 'no-cors' // Set mode to 'no-cors' to disable CORS
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        console.error('Error:', response.statusText);
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
 
-            const userData = { name, email };
-            localStorage.setItem('userData', JSON.stringify(userData));
-        } else {
-            handleInvalidCodeInput();
+                const userData = { name, email };
+                localStorage.setItem('userData', JSON.stringify(userData));
+            } else {
+                handleInvalidCodeInput();
+            }
+        } catch (error) {
+            console.error('Error fetching valid codes:', error);
         }
-    } catch (error) {
-        console.error('Error fetching valid codes:', error);
     }
-}
 
 
     function handleInvalidCodeInput() {
